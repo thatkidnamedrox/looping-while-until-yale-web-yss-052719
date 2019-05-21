bc55c501fccff0621f56f46c99063361fd43aaba
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-21 17:44:31 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,9 +1,9 @@
-def using_while(phrase)
+def using_while
   levitation_force = 6
 
   #your code here
   while levitation_force < 10
-    puts phrase
+    puts "Wingardium Leviosa"
     levitation_force+=1
   end
 
